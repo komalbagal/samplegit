00c75c29d1b7f1ebfa2095f8b2f03091720f0e3e
darsh <darsh@LAPTOP-AEIF858E>
2020-03-11 16:19:36 +0530

    first commit

---
+++ src/main/java/scp/com/git/GitDemo/App.java
@@ -0,0 +1,13 @@
+package scp.com.git.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
